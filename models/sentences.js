619f41bf8e1f1e80cb782c79c07a0b15d46b8d84
AndyGreene <andrewginenthal@gmail.com>
2016-06-16 13:31:34 -0400

    cloned for heroku re-deploy, update watson node modules + api credentials

--- models/sentences.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var sentenceSchema = mongoose.Schema({
-	content: String,
-	analysis: Object
-})
-
-var Sentence = mongoose.model('Sentence', sentenceSchema);
-module.exports = Sentence;
